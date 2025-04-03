0e62f22ac60cbf660fa1a9956e83f7cf1b7a35e9
Pablo Mora <pablo.morad@uanl.edu.mx>
2025-04-03 17:51:50 -0600

    regular expressions

--- regex_findall.py
+++ regex_findall.py
@@ -1,30 +1,39 @@
 import re
 
 
-regex_coincidencias = {
-    "numero_telefono": r"\b\d{3}[-.\s]?\d{3}[-.\s]?\d{4}\b",  
+regex_patterns = {
+    "numero_telefono": r"\b\d{3}[-.\s]?\d{3}[-.\s]?\d{4}\b",
     "cumpleaños": r"\b\d{2}/\d{2}/\d{4}\b|\b\d{4}-\d{2}-\d{2}\b",  
-    "contraseña": r"\b[a-zA-Z0-9]{8}\b",  
-    "sitio_web": r"https?://[^\s]+",  
-    "numero_tarjeta": r"\b(?:\d[ -]*?){13,16}\b", 
+    "contraseña_8_caracteres": r"\b[a-zA-Z0-9]{8}\b",  
+    "sitio_web": r"https://[^\s]+",  
+    "numero_tarjeta": r"\b(?:\d[ -]*?){13,16}\b",  
 }
 
-
 def buscar_en_texto(texto):
-   
-    for tipo, patron in regex_coincidencias.items():
-        coincidencias = re.findall(patron, texto)
-        print(f"{tipo} encontrados: {coincidencias}")
+    try:
+        for tipo, patron in regex_patterns.items():
+            try:
+                coincidencias = re.findall(patron, texto)
+                if coincidencias:
+                    print(f"{tipo} encontrados: {coincidencias}")
+                else:
+                    print(f"{tipo}: No se encontraron coincidencias.")
+            except re.error as e:
+                print(f"Error en la expresión regular para {tipo}: {e}")
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
 
-texto_de_ejemplo = """
-Número de teléfono: 123-456-7890
-Cumpleaños: 20/12/1979
-Contraseña: abcd1234, 
+
+ejemplos = """
+Numero de telefono: 123-456-7890 
+Cumpleaños: 05/12/1999
+Contraseña: ad203040
 Tarjeta de crédito: 1234 5678 9012 3456.
-sitio web:  https://www.ejemplo.com 
+Sitio web: https://www.sitioweb.com 
 """
 
-buscar_en_texto(texto_de_ejemplo)
+buscar_en_texto(ejemplos)
+
 
 
 
